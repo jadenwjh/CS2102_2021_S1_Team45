d7f6456cc354c9c73ecd8f68e70cda47835c1e4a
Jaden <jadenwong@mozat.com>
2020-10-26 18:25:24 +0800

    added callbacks in register

--- app/src/main/java/com/example/cs2102/view/CareTakerSignUpFragment.java
+++ app/src/main/java/com/example/cs2102/view/CareTakerSignUpFragment.java
@@ -1,22 +1,81 @@
 package com.example.cs2102.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.cs2102.R;
 
+import java.util.Objects;
+
 public class CareTakerSignUpFragment extends Fragment {
+
+    private EditText username;
+    private EditText password;
+    private EditText email;
+    private EditText number;
+    private EditText address;
+    private Button register;
+
+    private RegisterCTListener registerListener;
+
+    public interface RegisterCTListener {
+        void onExitRegister();
+
+        //move these as a separate interface
+        void onCheckCareTakerUsernameTaken(String username);
+        void onRegisterCareTaker(String username, String password, String email, String number, String address);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_care_taker_sign_up, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_care_taker_sign_up, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        username = (EditText) Objects.requireNonNull(getView()).findViewById(R.id.username);
+        password = (EditText) Objects.requireNonNull(getView()).findViewById(R.id.password);
+        email = (EditText) Objects.requireNonNull(getView()).findViewById(R.id.email);
+        number = (EditText) Objects.requireNonNull(getView()).findViewById(R.id.number);
+        address = (EditText) Objects.requireNonNull(getView()).findViewById(R.id.address);
+        register = (Button) Objects.requireNonNull(getView()).findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = username.getText().toString();
+                String pw = password.getText().toString();
+                String mail = email.getText().toString();
+                String num = number.getText().toString();
+                String add = address.getText().toString();
+
+                registerListener.onCheckCareTakerUsernameTaken(uname);
+                registerListener.onRegisterCareTaker(uname, pw, mail, num, add);
+                registerListener.onExitRegister();
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if (context instanceof RegisterCTListener) {
+            registerListener = (RegisterCTListener) context;
+        } else {
+            throw new ClassCastException("ExitRegisterListener not implemented");
+        }
     }
 
     @Override
--- app/src/main/java/com/example/cs2102/view/RegisterActivity.java
+++ app/src/main/java/com/example/cs2102/view/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -15,7 +16,7 @@ import com.example.cs2102.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements CareTakerSignUpFragment.RegisterCTListener {
 
     @BindView(R.id.pet_owner)
     Button petOwner;
@@ -26,11 +27,17 @@ public class RegisterActivity extends AppCompatActivity {
     @BindView(R.id.register)
     Button register;
 
+    private FragmentManager fm = getSupportFragmentManager();
+    private FragmentTransaction ft = fm.beginTransaction();
+    private Fragment careTakerFragment = fm.findFragmentByTag("CareTakerSignUp");
+    private Fragment petOwnerFragment = fm.findFragmentByTag("PetOwnerSignUp");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        switchFragment(1);
 
         petOwner.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,41 +52,24 @@ public class RegisterActivity extends AppCompatActivity {
                 switchFragment(2);
             }
         });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerUser();
-                backToLoginPage();
-            }
-        });
     }
 
     private void switchFragment(int id) {
-        FragmentManager fm = getSupportFragmentManager();
 
         switch (id) {
             case 1: {
-                Fragment careTakerFragment = fm.findFragmentByTag("CareTakerSignUp");
-                FragmentTransaction ft = fm.beginTransaction()
-                        .replace(R.id.register_form, new PetOwnerSignUpFragment(), "PetOwnerSignUp");
-
+                ft.replace(R.id.register_form, new PetOwnerSignUpFragment(), "PetOwnerSignUp");
                 if (careTakerFragment != null) {
                     ft.remove(careTakerFragment);
                 }
-
                 ft.commit();
                 break;
             }
             case 2: {
-                Fragment petOwnerFragment = fm.findFragmentByTag("PetOwnerSignUp");
-                FragmentTransaction ft = fm.beginTransaction()
-                        .replace(R.id.register_form, new CareTakerSignUpFragment(), "CareTakerSignUp");
-
+                ft.replace(R.id.register_form, new CareTakerSignUpFragment(), "CareTakerSignUp");
                 if (petOwnerFragment != null) {
                     ft.remove(petOwnerFragment);
                 }
-
                 ft.commit();
                 break;
             }
@@ -88,11 +78,23 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    private void registerUser() {}
-
-    private void backToLoginPage() {
+    @Override
+    public void onExitRegister() {
         Intent loginPage = new Intent(this, LoginActivity.class);
         startActivity(loginPage);
         finish();
+    }
+
+    //shift this to RegisterVM
+    @Override
+    public void onRegisterCareTaker(String username, String password, String email, String number, String address) {
+        Toast.makeText(this, "registered as CT", Toast.LENGTH_SHORT).show();
+        //Register
+    }
+
+    //shift this to RegisterVM
+    @Override
+    public void onCheckCareTakerUsernameTaken(String username) {
+        Toast.makeText(this, "Username is taken", Toast.LENGTH_SHORT).show();
     }
 }
