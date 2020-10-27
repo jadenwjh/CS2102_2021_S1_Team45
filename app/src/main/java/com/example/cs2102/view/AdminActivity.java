2ba7d64e4d17ec28ee1433b1775b03acfaea3bf1
Jaden <jadenwong@mozat.com>
2020-10-27 18:11:51 +0800

    refactor

---
+++ app/src/main/java/com/example/cs2102/view/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.example.cs2102.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cs2102.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
